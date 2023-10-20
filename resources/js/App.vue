62dbecf1a99e18737552e82f26bf5f37b3e1be2f
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-10-20 10:01:20 +0200

    setup for the exercice

---
+++ resources/js/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+    import { ref } from 'vue';
+    const data = ref([]);
+
+    axios.get('/api/users')
+    .then(function (response) {
+        // handle success
+        data.value = response.data.data;
+    })
+
+    console.log(data);
+</script>
+
+<template>
+<table>
+    <tr v-for="user in data">
+        <td>
+            <a @click="retrieveData">
+                {{ user.email }}
+            </a>
+        </td>
+    </tr>
+</table>
+
+</template>
+
+<style scoped>
+
+</style>
